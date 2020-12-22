0995d09ef26ac3c27a1565fd4070ca6059a56529
MaXOlimP <maxolimp7@gmail.com>
2020-12-22 20:21:15 +0100

    creat new react project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+            <div>one dive</div>
+            <div className="App">
+                <ul>
+                    <li>js</li>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>react</li>
+                    <li>jsx</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const title = () => {
+    return (
+        <div>
+            <ul>
+                <li> hea</li>
+            </ul>
+        </div>
+    );
+};
+export default App;
