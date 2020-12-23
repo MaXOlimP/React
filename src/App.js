6442cdcb48f4d9053973bd505e7daf8af775277d
MaXOlimP <maxolimp7@gmail.com>
2020-12-23 17:28:46 +0100

    starting poczatek

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
                     <li>jsx</li>
                 </ul>
             </div>
+            <Next />
         </div>
     );
 };
