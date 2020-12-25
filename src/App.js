5d15cf2865febcaff850b9c5dac00fe2cca9bc61
MaXOlimP <maxolimp7@gmail.com>
2020-12-25 23:39:08 +0100

    add new href &add new logo

--- src/App.js
+++ src/App.js
@@ -6,15 +6,33 @@ const App = () => {
         <div className="app-wrapper">
             <header className="header">
                 <img
-                    src="https://logomaster.ai/static/media/sample.c8bf2b04.svg"
-                    alt="Rick"
+                    src="https://www.sativa-poland.pl/assets/images/sativapoland-logo-1-1-298x256.png"
+                    alt="Sativa"
                 />
             </header>
             <nav className="nav">
-                <div>Profile</div>
-                <div>Messages</div>
+                <div>
+                    <a href="#s">Profile</a>
+                </div>
+                <div>
+                    <a href="#s">Messages</a>
+                </div>
+                <div>
+                    <a href="#s">News</a>
+                </div>
+                <div>
+                    <a href="#s">Music</a>
+                </div>
+                <div>
+                    <a href="#s">Settings</a>
+                </div>
             </nav>
-            <div className="content">Main content</div>
+            <div className="content">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzG54ww1YZLKXoOuClJvRIK_VA9TtrjS0hsQ&usqp=CAU://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmtzxDlnPTqkDlriVgoJm_EklsAe1uxEjrWA&usqp=CAU://ppcdn.500px.org/1794879/9d80693271620c05ff63610ece497b6659f7d6f3/4.jpg"
+                    alt=" India v Sativa v Hybrid"
+                />
+            </div>
         </div>
     );
 };
