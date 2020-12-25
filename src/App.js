e8b0c8ff10c2eaac56dfe3e13ce1cfcb40c1f2f6
MaXOlimP <maxolimp7@gmail.com>
2020-12-25 16:16:41 +0100

    eslintcache#3 & src modified

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header.js";
+import Technologies from "./Technologies.js";
 
 const App = () => {
     return (
         <div>
             <Header />
             <Technologies />
-        </div>
-    );
-};
-const Technologies = () => {
-    return (
-        <div>
-            <div>one dive</div>
-            <div className="App">
-                <ul>
-                    <li>js</li>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>react</li>
-                    <li>jsx</li>
-                </ul>
-            </div>
-        </div>
-    );
-};
-
-const Header = () => {
-    return (
-        <div>
-            <a href="#s">first project</a>
         </div>
     );
 };
