5cc7b9ea2b5e9a6034d8f4f34ee2601bc76328c8
MaXOlimP <maxolimp7@gmail.com>
2020-12-25 21:56:55 +0100

    create new site & start to header navigation and content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header.js";
-import Technologies from "./Technologies.js";
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://logomaster.ai/static/media/sample.c8bf2b04.svg"
+                    alt="Rick"
+                />
+            </header>
+            <nav className="nav">
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div className="content">Main content</div>
         </div>
     );
 };
