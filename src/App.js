6ec38971674461b00adb256ee484d774daee4796
MaXOlimP <maxolimp7@gmail.com>
2020-12-26 19:11:27 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Content from "./components/Content";
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src="https://www.sativa-poland.pl/assets/images/sativapoland-logo-1-1-298x256.png"
-                    alt="Sativa"
-                />
-            </header>
-            <nav className="nav">
-                <div>
-                    <a href="#s">Profile</a>
-                </div>
-                <div>
-                    <a href="#s">Messages</a>
-                </div>
-                <div>
-                    <a href="#s">News</a>
-                </div>
-                <div>
-                    <a href="#s">Music</a>
-                </div>
-                <div>
-                    <a href="#s">Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzG54ww1YZLKXoOuClJvRIK_VA9TtrjS0hsQ&usqp=CAU://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmtzxDlnPTqkDlriVgoJm_EklsAe1uxEjrWA&usqp=CAU://ppcdn.500px.org/1794879/9d80693271620c05ff63610ece497b6659f7d6f3/4.jpg"
-                    alt=" India v Sativa v Hybrid"
-                />
-                <div>
-                    <div>Avatar + description</div>
-                    <div>My Post</div>
-                    <div>New Post</div>
-                    <div>post 1</div>
-                    <div>post 2</div>
-                </div>
-            </div>
+            <Header />
+            <NavBar />
+            <Content />
         </div>
     );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://www.sativa-poland.pl/assets/images/sativapoland-logo-1-1-298x256.png"
+                alt="Sativa"
+            />
+        </header>
+    );
+};
+
+export default Header;
