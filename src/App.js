f15cb2541132ea68cbdac77615286e67de12ea8c
MaXOlimP <maxolimp7@gmail.com>
2020-12-26 18:55:00 +0100

    src change

--- src/App.js
+++ src/App.js
@@ -32,6 +32,13 @@ const App = () => {
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzG54ww1YZLKXoOuClJvRIK_VA9TtrjS0hsQ&usqp=CAU://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmtzxDlnPTqkDlriVgoJm_EklsAe1uxEjrWA&usqp=CAU://ppcdn.500px.org/1794879/9d80693271620c05ff63610ece497b6659f7d6f3/4.jpg"
                     alt=" India v Sativa v Hybrid"
                 />
+                <div>
+                    <div>Avatar + description</div>
+                    <div>My Post</div>
+                    <div>New Post</div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
             </div>
         </div>
     );
